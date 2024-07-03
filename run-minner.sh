7718ebc4c8ccb8f589d2195e82999d6920fac837
Thien Tran <fcduythien@gmail.com>
2024-07-03 14:11:59 +0700

    Update run-minner.sh

--- run-minner.sh
+++ run-minner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 sudo apt-get install screen unzip psmisc  -y
+chmod +x tnn-miner-v0.3.6
 screen -dmS node ./tnn-miner-v0.3.6 --spectre --daemon-address spr.tw-pool.com --port 14001 --wallet spectre:qqzqrthexh8tv5eg6sc4jp6gnuxg5gt4ahjnj0558s2nc9nsw78ywc43szyk0  --threads 15 --worker test-openstack-cpu1
